e6fdf6b9dd82c86962026e199bdce3ab0af05373
J. Avila <elavila@google.com>
2021-04-12 18:22:36 +0000

    rubyx: Explicitly set log_buf_len
    By setting the log_buf_len via cmdline, devices using this cmdline will
    be able to guarantee their preferred buffer size even if the underlying
    CONFIG_LOG_BUF_SHIFT changes.
    
    Bug: 185141673
    Change-Id: Iafe7561dceef65fc47cb0bb54a8f84a5e10c51a9
    Signed-off-by: rajdeep-3305 <rajdeepbiswas3305@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,6 +68,7 @@ BOARD_KERNEL_CMDLINE := \
 BOARD_KERNEL_CMDLINE += cgroup_disable=memory
 BOARD_KERNEL_CMDLINE += androidboot.serialconsole=0
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+BOARD_KERNEL_CMDLINE += log_buf_len=1024K
 
 BOARD_MKBOOTIMG_ARGS := --base $(BOARD_KERNEL_BASE)
 BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
