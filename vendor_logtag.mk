52a96f9a536efe4b92236e9410b973c8ee26f983
ChrisCatto <chriscatto@proton.me>
2024-12-10 08:27:06 +0200

    rubyx: Silence GPUAUX logspam
    Change-Id: I7d1b08758a9e770d5252973304c5367a6769de44
    Signed-off-by: ChrisCatto <chriscatto@proton.me>
    Signed-off-by: rajdeep-3305 <rajdeepbiswas3305@gmail.com>

--- vendor_logtag.mk
+++ vendor_logtag.mk
@@ -45,6 +45,7 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.log.tag.GsmCdmaConn=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.GsmCdmaPhone=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.hwcomposer=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.GPUAUX=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.HWUI=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.IccCardProxy=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.IccPhoneBookIM=$(VENDOR_LOG_LEVEL) \
