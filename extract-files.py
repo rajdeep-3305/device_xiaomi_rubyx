356f5b7795405d68d1161e47d17957086a31e59b
rio004 <riyon336@gmail.com>
2026-03-14 23:10:41 +0530

    rubyx: dolby: Drop dolby stack
    * migrate to hardware/dolby
    
    Change-Id: I3617786bf5f9c76eec09e1ad66049defcc42c8da
    Co-authored-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Signed-off-by: rajdeep-3305 <rajdeepbiswas3305@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -35,15 +35,18 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'vendor/bin/mi_thermald': blob_fixup()
         .binary_regex_replace(b'%d/on', b'%d/..'),
+    
     ('vendor/bin/hw/android.hardware.gnss-service.mediatek',
-    'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'): blob_fixup()
+     'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'): blob_fixup()
         .replace_needed('android.hardware.gnss-V1-ndk_platform.so', "android.hardware.gnss-V1-ndk.so"),
     
     'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b': blob_fixup()
         .add_needed('libstagefright_foundation-v33.so')
         .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so'),
+        
     'vendor/etc/init/android.hardware.neuralnetworks@1.3-service-mtk-neuron.rc': blob_fixup()
         .regex_replace('start', 'enable'),
+        
     ('vendor/firmware/txpowerctrl_gl.cfg',
      'vendor/firmware/txpowerctrl_gl_u.cfg',
      'vendor/firmware/txpowerctrl_in.cfg',
@@ -51,7 +54,8 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace(r'\t', ''),
     
     'vendor/etc/vintf/manifest/manifest_media_c2_V1_2_default.xml': blob_fixup()
-        .regex_replace('1.1', '1.2'),
+        .regex_replace('1.1', '1.2')
+        .regex_replace('.+dolby.+\n', ''),
     
     ('vendor/bin/mnld',
      'vendor/lib/libaalservice.so',
@@ -84,11 +88,6 @@ blob_fixups: blob_fixups_user_type = {
      'vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so'): blob_fixup()
         .replace_needed('libutils.so', 'libutils-v32.so'),
     
-    ('vendor/lib64/libdlbdsservice.so',
-     'vendor/lib/soundfx/libswdap.so',
-     'vendor/lib64/soundfx/libswdap.so'): blob_fixup()
-        .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
-
     ('vendor/lib/libnvram.so',
      'vendor/lib64/libnvram.so',
      'vendor/lib64/libsysenv.so',
@@ -101,7 +100,7 @@ blob_fixups: blob_fixups_user_type = {
     ('vendor/lib/librt_extamp_intf.so',
      'vendor/lib/hw/audio.primary.mt6877.so',
      'vendor/lib64/hw/vendor.mediatek.hardware.pq@2.14-impl.so'): blob_fixup()
-	    .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
 
     'vendor/etc/init/android.hardware.neuralnetworks@1.3-service-mtk-neuron.rc': blob_fixup()
         .regex_replace('start', 'enable'),
@@ -112,9 +111,9 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('__gnu_Unwind_Find_exidx'),
         
     ('vendor/lib64/libcam.hal3a.v3.so',
-    'vendor/lib64/libeffecthal.base.so',
-    'vendor/lib64/libmtkcam_grallocutils.so',
-    'vendor/lib64/libmtkcam_3rdparty.vidhance.so'): blob_fixup()
+     'vendor/lib64/libeffecthal.base.so',
+     'vendor/lib64/libmtkcam_grallocutils.so',
+     'vendor/lib64/libmtkcam_3rdparty.vidhance.so'): blob_fixup()
         .replace_needed('libui.so', 'libui-v34.so'),
 }
 
