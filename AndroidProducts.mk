64e9830eccf16c2ab415e341367e89d98979e597
rajdeep-3305 <rajdeepbiswas3305@gmail.com>
2026-09-13 19:38:04 +0000

    rubyx: configs: Add custom_rubyx product makefile for PixelOS
    - Define custom_rubyx target inheriting from PixelOS common configuration
    - Set device fingerprint, branding, and maintainer metadata
    - Add custom_rubyx-userdebug to lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,4 +4,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_rubyx.mk
+    $(LOCAL_DIR)/custom_rubyx.mk
+
+COMMON_LUNCH_CHOICES := \
+    custom_rubyx-userdebug
---
+++ custom_rubyx.mk
@@ -0,0 +1,35 @@
+#
+# SPDX-FileCopyrightText: 2023-2025 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/xiaomi/rubyx/device.mk)
+
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
+
+PRODUCT_NAME := custom_rubyx
+PRODUCT_DEVICE := rubyx
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := ruby
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildFingerprint=Redmi/ruby_global/ruby:14/UP1A.230620.001/OS2.0.15.0.UMOMIXM:user/release-keys \
+    SystemName=ruby_global \
+    SystemDevice=ruby
+
+# Axion Stuff
+TARGET_ENABLE_BLUR := false
+AXION_CAMERA_REAR_INFO := 200/50,8,2
+AXION_CAMERA_FRONT_INFO := 16
+AXION_MAINTAINER := Casanova.
+AXION_PROCESSOR := MediaTek_Dimensity_1080_(MT6877V)_(6nm)
+TARGET_INCLUDES_LOS_PREBUILTS := true
